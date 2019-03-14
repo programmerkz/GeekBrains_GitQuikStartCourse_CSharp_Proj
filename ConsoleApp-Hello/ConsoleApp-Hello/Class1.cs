717c1cf6713a3f60358399f693488d1b7539387e
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-14 11:17:55 +0600

    протестировал создание пустого класса, состоящего только из констант

---
+++ ConsoleApp-Hello/ConsoleApp-Hello/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp_Hello
+{
+    static class Class1
+    {
+        public const string MY_NAME = "Sanya";
+    }
+}
--- ConsoleApp-Hello/ConsoleApp-Hello/Program.cs
+++ ConsoleApp-Hello/ConsoleApp-Hello/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp_Hello
         {
             // вывести слово Привет на экран и ожидать нажания клавиши Enter
 
-            Console.WriteLine("Privet!");
+            Console.WriteLine("Privet! " + Class1.MY_NAME);
             Console.WriteLine("\r\n press Enter to continue");
             Console.ReadLine();
         }
